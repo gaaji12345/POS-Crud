6e1a148348cfc52b43d2b2770b88bbf7a3507aa6
gaji12345 <gajimax12345@gmail.com>
2024-02-08 16:15:54 +0530

    impliment CustomerSave Method

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -1,8 +1,10 @@
 package controller;
 
+import entity.Customer;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import repo.CustomerRepo;
 
 public class CustomerFormController {
     public TextField txtID;
@@ -12,6 +14,10 @@ public class CustomerFormController {
     public TableView tblCustomer;
 
     public void save_OnAction(ActionEvent actionEvent) {
+        Customer customer=new Customer(txtID.getText(),txtName.getText(),txtAddress.getText(),Integer.parseInt(txtTel.getText()));
+        CustomerRepo customerRepo=new CustomerRepo();
+        String  Cid=customerRepo.saveCustomer(customer);
+        System.out.println("Save Customer Id"+Cid);
     }
 
     public void update_OnActon(ActionEvent actionEvent) {
