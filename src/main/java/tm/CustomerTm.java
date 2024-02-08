bbf06e9b09dd4ffe53c22ebe3725e168cdd30824
gaji12345 <gajimax12345@gmail.com>
2024-02-08 16:25:42 +0530

    Customer Tm USE FOR LOADtbale

---
+++ src/main/java/tm/CustomerTm.java
@@ -0,0 +1,14 @@
+package tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor@NoArgsConstructor
+@Data
+public class CustomerTm {
+    private String id;
+    private String name;
+    private  String  address;
+    private int tel;
+}
