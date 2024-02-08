7656c4c56c27b6e7c21083302d28ae4a5a160493
gaji12345 <gajimax12345@gmail.com>
2024-02-08 21:08:23 +0530

    dto and entity from items

--- src/main/java/entity/Item.java
+++ src/main/java/entity/Item.java
@@ -1,16 +1,20 @@
-package dto;
+package entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Itemdto {
+@Entity
+public class Item {
+    @Id
     private String code;
     private  String name;
     private  double price;
     private int qty;
-
 }
