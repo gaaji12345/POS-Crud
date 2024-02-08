365b21a0a918d4eb4349ad70092efcb53329b43a
gaji12345 <gajimax12345@gmail.com>
2024-02-08 16:02:46 +0530

    first commit

---
+++ src/main/java/controller/CustomerFormController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerFormController {
+    public TextField txtID;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtTel;
+    public TableView tblCustomer;
+
+    public void save_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void update_OnActon(ActionEvent actionEvent) {
+    }
+
+    public void delete_OnAction(ActionEvent actionEvent) {
+    }
+}
